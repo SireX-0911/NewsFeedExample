8a5ae9c25f55227765805d36216278ed537ca4af
Sirojiddin <eragon0911@gmail.com>
2018-08-16 15:40:22 +0500

    commit

--- app/src/main/java/com/example/sirojiddin/testapplication/utils/FileTypeUtils.kt
+++ app/src/main/java/com/example/sirojiddin/testapplication/utils/FileTypeUtils.kt
@@ -1,12 +1,18 @@
 package com.example.sirojiddin.testapplication.utils
 
 import android.graphics.BitmapFactory
+import org.intellij.lang.annotations.RegExp
+import java.util.regex.Pattern
 
 object FileTypeUtils {
     fun isImage(path: String) : Boolean {
-        val options =  BitmapFactory.Options()
-        options.inJustDecodeBounds = true
-        BitmapFactory.decodeFile(path, options)
-        return (options.outWidth != -1 && options.outHeight != -1)
+//        val options =  BitmapFactory.Options()
+//        options.inJustDecodeBounds = true
+//        BitmapFactory.decodeFile(path, options)
+//        return (options.outWidth != -1 && options.outHeight != -1)
+        val regex = "(?:([^:/?#]+):)?(?://([^/?#]*))?([^?#]*\\.(?:jpg|gif|png))(?:\\?([^#]*))?(?:#(.*))?"
+        val pattern = Pattern.compile(regex)
+        val matcher = pattern.matcher(path)
+        return matcher.matches()
     }
 }
